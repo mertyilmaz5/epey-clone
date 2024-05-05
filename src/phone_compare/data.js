072c88a4b5f58a55f8baf26fa3c95f7f11404711
mythie-git <mryilmaz005@gmail.com>
2024-05-05 18:18:27 +0300

    Added comparison

---
+++ src/phone_compare/data.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    brand: "Apple",
+    model: "iPhone 15 Pro Max",
+    img: "https://resim.epey.com/894486/m_apple-iphone-15-pro-max-3.png",
+    features: [
+      {
+        title: "Fiyat",
+        text: "74.000 TL",
+        value: 74000,
+      },
+      {
+        title: "Ekran Boyutu",
+        text: "6.7 inç",
+        value: 6.7,
+      },
+      {
+        title: "Kamera Çözünürlüğü",
+        text: "48 MP",
+        value: 48,
+      },
+      {
+        title: "Dahili Depolama",
+        text: "256 GB",
+        value: 256,
+      },
+      {
+        title: "CPU Frekansı",
+        text: "3.64 GHz",
+        value: 3.64,
+      },
+    ],
+  },
+  {
+    brand: "Apple",
+    model: "iPhone 13",
+    img: "https://resim.epey.com/716503/m_apple-iphone-13-10.png",
+    features: [
+      {
+        title: "Fiyat",
+        text: "35.500 TL",
+        value: 35500,
+      },
+      {
+        title: "Ekran Boyutu",
+        text: "6.1 inç",
+        value: 6.1,
+      },
+      {
+        title: "Kamera Çözünürlüğü",
+        text: "12 MP",
+        value: 12,
+      },
+      {
+        title: "Dahili Depolama",
+        text: "125 GB",
+        value: 125,
+      },
+      {
+        title: "CPU Frekansı",
+        text: "3.2 GHz",
+        value: 3.2,
+      },
+    ],
+  },
+];
+
+export default data;
